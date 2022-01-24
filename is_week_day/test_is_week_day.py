02ac09b46958fb39b3f9a3263684d723d4616b72
coutinho82 <87770322+coutinho82@users.noreply.github.com>
2022-01-24 16:48:42 +0000

    error test
    test if error prevents merge

--- is_week_day/test_is_week_day.py
+++ is_week_day/test_is_week_day.py
@@ -10,8 +10,7 @@ class Test_Week_Day(unittest.TestCase):
         # is_weekday = IsWeekDay() # Arrange
         return_value = IsWeekDay.check_if_today_is_a_weekday(today)  # Act
         self.assertTrue(return_value)  # Assert
-
-    def test_assert_currentdate_not_a_weekday(self):
+def test_assert_currentdate_not_a_weekday(self):
         today = datetime.datetime(2022, 1, 16)
         # is_weekday = IsWeekDay() # Arrange
         return_value = IsWeekDay.check_if_today_is_a_weekday(today)  # Act
