09d679965577a13d895bd7ccad75e931fa718fef
Alex Scott <alexanderscott@outlook.com>
2022-01-24 15:25:42 +0000

    added new parameters

--- main_yaml_loader.py
+++ main_yaml_loader.py
@@ -4,12 +4,14 @@ from yaml_loader.yaml_saver import YamlSaver
 import argparse
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('job_name', type=str, help='its a name for existing job')
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument("job_name", type=str, help="its a name for existing job")
+    parser.add_argument("read_file_name", type=str, help="its a name for existing file")
+    parser.add_argument("write_file_name", type=str, help="its a name for new file")
     args = parser.parse_args()
-    data_info = YamlLoader.read_file("yaml_loader/data.yaml")
+    data_info = YamlLoader.read_file("yaml_loader/" + args.read_file_name)
     print(data_info)
     changed_job = YamlModify.yaml_modify(data_info, args.job_name)
     print(changed_job)
-    YamlSaver.save_file(changed_job, "yaml_loader/new_data.yaml")
-    print(YamlLoader.read_file("yaml_loader/new_data.yaml"))
+    YamlSaver.save_file(changed_job, "yaml_loader/" + args.write_file_name)
+    print(YamlLoader.read_file("yaml_loader/" + args.write_file_name))
