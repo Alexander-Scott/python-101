783f8dea0d74519b61d8b4783e2e966acf444db8
Alex Scott <alexanderscott@outlook.com>
2022-01-24 11:22:18 +0000

    Adding a loop to run tru

--- main_yaml_loader.py
+++ main_yaml_loader.py
@@ -1,5 +1,7 @@
 from yaml_loader.yaml_loader import YamlLoader
+from yaml_loader.yaml_modify import YamlModify
 
 if __name__ == "__main__":
     data_info = YamlLoader.read_file("yaml_loader/data.yaml")
     print(data_info)
+    YamlModify.yaml_modify(data_info)
--- yaml_loader/yaml_modify.py
+++ yaml_loader/yaml_modify.py
@@ -1,8 +1,27 @@
 import os
 
+
 class YamlModify:
     @staticmethod
-    def yaml_modify(loader_file: list, key: dict):
-        # list = [{'job': {'name': 'my-job', 'parent': 'my-parent', 'vars': {'data': 'hello world'}}}]
+    def yaml_modify(loader_file: list):
+        # list = [
+        #     {"job": {"name": "my-job", "parent": "my-parent", "vars": {"data": "hello world"}}},
+        #     {"job": {"name": "my-job_2", "parent": "my-job", "vars": {"data": "hello world"}}},
+        # ]
         list = loader_file
-        
+        for job in loader_file:
+            for key in job["job"]:
+                print("what vaue for  " + key)
+                job["job"][key] = 
+                if isinstance(job["job"][key], dict):
+                    for nested_key in job["job"][key]:
+                        print("what vaue for  " + nested_key)
+
+
+# job["job"] = {"name": "my-job", "parent": "my-parent", "vars": {"data": "hello world"}}
+# job["job"]["name"] = "my-job"
+# job["job"]["vars"] = {"data": "hello world", "daata2": "fsfdsf"}
+
+# for job in loader_file:
+# .  print(job)
+# .  print(job["name"])
