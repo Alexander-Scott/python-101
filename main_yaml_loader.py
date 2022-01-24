726047f47065352fd0c51745f30744cd4cf5c5b6
Alex Scott <alexanderscott@outlook.com>
2022-01-24 15:18:52 +0000

    input from user for job name

--- main_yaml_loader.py
+++ main_yaml_loader.py
@@ -1,11 +1,15 @@
 from yaml_loader.yaml_loader import YamlLoader
 from yaml_loader.yaml_modify import YamlModify
 from yaml_loader.yaml_saver import YamlSaver
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('job_name', type=str, help='its a name for existing job')
+    args = parser.parse_args()
     data_info = YamlLoader.read_file("yaml_loader/data.yaml")
     print(data_info)
-    changed_job = YamlModify.yaml_modify(data_info)
+    changed_job = YamlModify.yaml_modify(data_info, args.job_name)
     print(changed_job)
     YamlSaver.save_file(changed_job, "yaml_loader/new_data.yaml")
     print(YamlLoader.read_file("yaml_loader/new_data.yaml"))
