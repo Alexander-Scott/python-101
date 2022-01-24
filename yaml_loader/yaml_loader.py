8d9fa1e66e23896322942644ccee849b847d573c
Alex Scott <alexanderscott@outlook.com>
2022-01-24 11:22:18 +0000

    Adding a loop to run tru

--- yaml_loader/yaml_loader.py
+++ yaml_loader/yaml_loader.py
@@ -1,8 +1,9 @@
 import yaml, os
 
+
 class YamlLoader:
     @staticmethod
     def read_file(file_path) -> dict:
-        with open(file_path,"r") as file:
+        with open(file_path, "r") as file:
             file_content = file.read()
             return yaml.load(file_content, Loader=yaml.FullLoader)
