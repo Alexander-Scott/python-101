da922279359449653ba7defab206575ad2c9cc1a
Alex Scott <alexanderscott@outlook.com>
2022-01-24 11:47:50 +0000

    creat a yaml file saver

---
+++ yaml_loader/yaml_saver.py
@@ -0,0 +1,9 @@
+import os, yaml
+
+
+class YamlSaver:
+    @staticmethod
+    def save_file(file_content: list, file_path: str):
+        with open(file_path, "w") as new_file:
+            # new_file.write(yaml.dump(file_content))
+            yaml.dump(file_content, new_file) #convert in to a string
