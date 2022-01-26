f3940cadef9159649ebe7c82fb63d948d4933ebb
Alex Scott <alexanderscott@outlook.com>
2022-01-26 10:30:07 +0000

    git fetch pull requests code coverage

--- github_pr_fetcher/pr_title_fetcher.py
+++ github_pr_fetcher/pr_title_fetcher.py
@@ -1,23 +1,33 @@
 import requests
-#import json
 
-pr_query_url = "https://api.github.com/repos/bagaco-ramp-up/python-101/pulls"
+# import json
+
+
+class NotFoundException(Exception):
+    pass
+
+
 class HttpGitHubFetchPRGit:
+    pr_query_url = "https://api.github.com/repos/bagaco-ramp-up/python-101/pulls"
+
     @staticmethod
     def get_github_pr_title():
-        pr_query_url = "https://api.github.com/repos/bagaco-ramp-up/python-101/pulls"
+        # pr_query_url = "https://api.github.com/repos/bagaco-ramp-up/python-101/pulls"
         pr_params = {
             "state": "all",
         }
-        pull_request_data = requests.get(pr_query_url, params=pr_params)
-        #print(pull_request_data)
+        pull_request_data = requests.get(HttpGitHubFetchPRGit.pr_query_url, params=pr_params)
+        if pull_request_data.status_code == 404:
+            raise NotFoundException("Link not found")
+        # print(pull_request_data)
         pull_request_dic = pull_request_data.json()
-        #print(json.dumps(pull_request_dic, indent=4, sort_keys=True))
-        for i in range(0,len(pull_request_dic)):
-                pull_request_title = pull_request_dic[i]["title"]
-                pull_request_state = pull_request_dic[i]["state"]
-                pull_request_number = pull_request_dic[i]["number"]
-                print(pull_request_number , " "+ pull_request_title + " "+ pull_request_state)
+        # print(json.dumps(pull_request_dic, indent=4, sort_keys=True))
+        for i in range(0, len(pull_request_dic)):
+            pull_request_title = pull_request_dic[i]["title"]
+            pull_request_state = pull_request_dic[i]["state"]
+            pull_request_number = pull_request_dic[i]["number"]
+            print(pull_request_number, " " + pull_request_title + " " + pull_request_state)
+        return pull_request_title
 
 
 # /repos/{owner}/{repo}/pulls/{pull_number}
